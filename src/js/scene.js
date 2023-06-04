586779bd7da860d760939f5c846d369698d04808
André Torres <andre.torres@tecnico.ulisboa.pt>
2023-06-04 19:16:15 +0100

    style: tab indentation

--- src/js/scene.js
+++ src/js/scene.js
@@ -39,100 +39,100 @@ var tree;
 
 
 function createOvni(x, y, z) {
-    "use strict";
+	"use strict";
 
-    var body, cockpit, spotlight, light1, light2, light3, light4, light5, light6, light7, light8;
+	var body, cockpit, spotlight, light1, light2, light3, light4, light5, light6, light7, light8;
 
-    // body
-    geometry = new THREE.SphereGeometry(15, 32, 32);
-    geometry.scale(1, 0.2, 1);
-    body = new THREE.Mesh(geometry, materials.get("gray"));
-    body.position.set(0, 0, 0);
+	// body
+	geometry = new THREE.SphereGeometry(15, 32, 32);
+	geometry.scale(1, 0.2, 1);
+	body = new THREE.Mesh(geometry, materials.get("gray"));
+	body.position.set(0, 0, 0);
 
-    // cockpit
-    geometry = new THREE.SphereGeometry(4, 32, 32);
-    cockpit = new THREE.Mesh(geometry, materials.get("lightGray"));
-    cockpit.position.set(0, 3, 0);
+	// cockpit
+	geometry = new THREE.SphereGeometry(4, 32, 32);
+	cockpit = new THREE.Mesh(geometry, materials.get("lightGray"));
+	cockpit.position.set(0, 3, 0);
 
-    // spotlight
-    geometry = new THREE.CylinderGeometry(7, 7, 1, 32);
-    spotlight = new THREE.Mesh(geometry, materials.get("yellow"));
-    spotlight.position.set(0, 0, 0);
+	// spotlight
+	geometry = new THREE.CylinderGeometry(7, 7, 1, 32);
+	spotlight = new THREE.Mesh(geometry, materials.get("yellow"));
+	spotlight.position.set(0, 0, 0);
 
-    var sLight = new THREE.SpotLight( 0xffffff );
-    sLight.castShadow = true;
+	var sLight = new THREE.SpotLight( 0xffffff );
+	sLight.castShadow = true;
 
-    // 8 lights around the cockpit
-    geometry = new THREE.CylinderGeometry(0.5, 0.5, 0.5, 32);
-    light1 = new THREE.Mesh(geometry, materials.get("yellow"));
-    light2 = new THREE.Mesh(geometry, materials.get("yellow"));
-    light3 = new THREE.Mesh(geometry, materials.get("yellow"));
-    light4 = new THREE.Mesh(geometry, materials.get("yellow"));
-    light5 = new THREE.Mesh(geometry, materials.get("yellow"));
-    light6 = new THREE.Mesh(geometry, materials.get("yellow"));
-    light7 = new THREE.Mesh(geometry, materials.get("yellow"));
-    light8 = new THREE.Mesh(geometry, materials.get("yellow"));
+	// 8 lights around the cockpit
+	geometry = new THREE.CylinderGeometry(0.5, 0.5, 0.5, 32);
+	light1 = new THREE.Mesh(geometry, materials.get("yellow"));
+	light2 = new THREE.Mesh(geometry, materials.get("yellow"));
+	light3 = new THREE.Mesh(geometry, materials.get("yellow"));
+	light4 = new THREE.Mesh(geometry, materials.get("yellow"));
+	light5 = new THREE.Mesh(geometry, materials.get("yellow"));
+	light6 = new THREE.Mesh(geometry, materials.get("yellow"));
+	light7 = new THREE.Mesh(geometry, materials.get("yellow"));
+	light8 = new THREE.Mesh(geometry, materials.get("yellow"));
 
-    // full ovni
-    ovni = new THREE.Object3D();
-    ovni.add(body, cockpit, spotlight, light1, light2, light3, light4, light5, light6, light7, light8);
+	// full ovni
+	ovni = new THREE.Object3D();
+	ovni.add(body, cockpit, spotlight, light1, light2, light3, light4, light5, light6, light7, light8);
 
-    // position the lights
-    for(var i = 0; i < 8; i++) {
-        var angle = i * Math.PI / 4;
-        var x = 11 * Math.cos(angle);
-        var z = 11 * Math.sin(angle);
-        ovni.children[i + 3].position.set(x, 0, z);
-        var light = new THREE.PointLight( 0xff0000, 1);
-        light.position.set( 50, 50, 50 );
-    }
+	// position the lights
+	for(var i = 0; i < 8; i++) {
+		var angle = i * Math.PI / 4;
+		var x = 11 * Math.cos(angle);
+		var z = 11 * Math.sin(angle);
+		ovni.children[i + 3].position.set(x, 0, z);
+		var light = new THREE.PointLight( 0xff0000, 1);
+		light.position.set( 50, 50, 50 );
+	}
 
-    ovni.position.set(x, y, z);
+	ovni.position.set(x, y, z);
 
-    scene.add(ovni);
+	scene.add(ovni);
 }
 
 function rotateOvni() {
-    "use strict";
+	"use strict";
 
-    ovni.rotation.y += Math.PI / 180;
+	ovni.rotation.y += Math.PI / 180;
 }
 
 function moveOvniX(left) {
-    "use strict";
-    var velocity = new THREE.Vector3(35, 0, 0).multiplyScalar(delta);
+	"use strict";
+	var velocity = new THREE.Vector3(35, 0, 0).multiplyScalar(delta);
 
-    if (left) {
-        velocity.x = -velocity.x;
-    } else {
-        velocity.x = velocity.x;
-    }
+	if (left) {
+		velocity.x = -velocity.x;
+	} else {
+		velocity.x = velocity.x;
+	}
 
-    ovni.position.add(velocity);
+	ovni.position.add(velocity);
 }
 
 function moveOvniZ(left) {
-    "use strict";
-    var velocity = new THREE.Vector3(0, 0, 35).multiplyScalar(delta);
-    
-    if (left) {
-        velocity.z = -velocity.z;
-    } else {
-        velocity.z = velocity.z;
-    }
+	"use strict";
+	var velocity = new THREE.Vector3(0, 0, 35).multiplyScalar(delta);
+	
+	if (left) {
+		velocity.z = -velocity.z;
+	} else {
+		velocity.z = velocity.z;
+	}
 
-    ovni.position.add(velocity);
+	ovni.position.add(velocity);
 }
 
 function createTree(x, y, z) {
-    "use strict";
+	"use strict";
 
-    var trunk, branch1, topBranch1, branch2, topBranch2, branch3, foliage1, foliage2, foliage3;
+	var trunk, branch1, topBranch1, branch2, topBranch2, branch3, foliage1, foliage2, foliage3;
 
-    // trunk
-    geometry = new THREE.CylinderGeometry(1.1, 1.5, 8, 16);
-    trunk = new THREE.Mesh(geometry, materials.get("peru"));
-    trunk.position.set(0, 4, 0); // base on the ground
+	// trunk
+	geometry = new THREE.CylinderGeometry(1.1, 1.5, 8, 16);
+	trunk = new THREE.Mesh(geometry, materials.get("peru"));
+	trunk.position.set(0, 4, 0); // base on the ground
 
 	// branch 1
 	geometry = new THREE.CylinderGeometry(0.7, 1, 6, 16);
@@ -170,32 +170,32 @@ function createTree(x, y, z) {
 	branch3.position.set(2, 12.1, 1.7);
 
 	// foliage 1
-    geometry = new THREE.SphereGeometry(6, 16, 16);
-    geometry.scale(0.9, 0.5, 1);
-    foliage1 = new THREE.Mesh(geometry, materials.get("darkOlive"));
+	geometry = new THREE.SphereGeometry(6, 16, 16);
+	geometry.scale(0.9, 0.5, 1);
+	foliage1 = new THREE.Mesh(geometry, materials.get("darkOlive"));
 	foliage1.rotation.x = THREE.MathUtils.degToRad(3);
-    foliage1.position.set(-1.5, 14, -4);
+	foliage1.position.set(-1.5, 14, -4);
 
 	// foliage 2
-    geometry = new THREE.SphereGeometry(7, 16, 16);
-    geometry.scale(0.8, 0.4, 1);
-    foliage2 = new THREE.Mesh(geometry, materials.get("darkGreen"));
+	geometry = new THREE.SphereGeometry(7, 16, 16);
+	geometry.scale(0.8, 0.4, 1);
+	foliage2 = new THREE.Mesh(geometry, materials.get("darkGreen"));
 	foliage2.rotation.z = THREE.MathUtils.degToRad(5);
-    foliage2.position.set(1, 15.5, 7);
+	foliage2.position.set(1, 15.5, 7);
 
 	// foliage 3
-    geometry = new THREE.SphereGeometry(4, 16, 16);
-    geometry.scale(1, 0.6, 1);
-    foliage3 = new THREE.Mesh(geometry, materials.get("olive"));
-    foliage3.position.set(4, 15, 0);
+	geometry = new THREE.SphereGeometry(4, 16, 16);
+	geometry.scale(1, 0.6, 1);
+	foliage3 = new THREE.Mesh(geometry, materials.get("olive"));
+	foliage3.position.set(4, 15, 0);
 
-    // full tree
-    tree = new THREE.Object3D();
-    tree.add(trunk, branch1, topBranch1, branch2, topBranch2, branch3, foliage1, foliage2, foliage3);
+	// full tree
+	tree = new THREE.Object3D();
+	tree.add(trunk, branch1, topBranch1, branch2, topBranch2, branch3, foliage1, foliage2, foliage3);
 
-    tree.position.set(x, y, z);
+	tree.position.set(x, y, z);
 
-    scene.add(tree);
+	scene.add(tree);
 }
 
 function createScene() {
@@ -204,7 +204,7 @@ function createScene() {
 	scene = new THREE.Scene();
 	scene.background = new THREE.Color("rgb(230, 230, 230)");
 
-    createOvni(0, 40, 0);
+	createOvni(0, 40, 0);
 	createTree(0, 0, 0);
 }
 
@@ -270,7 +270,7 @@ function onKeyDown(e) {
 		// 1-5
 		currentCameraIndex = e.keyCode - 49;
 	}
-    // Toggle wireframe
+	// Toggle wireframe
 	else if (e.keyCode == 54) {
 		// 6
 		for (let material of materials.values()) {
@@ -313,7 +313,7 @@ function update() {
 		console.log("Q");
 	}
 
-    if (keys[37] == 1) {
+	if (keys[37] == 1) {
 		console.log("L-Arrow");
 		moveOvniX(true);
 	}
@@ -330,7 +330,7 @@ function update() {
 		moveOvniZ(false);
 	}
 
-    rotateOvni();
+	rotateOvni();
 }
 
 function animate() {
